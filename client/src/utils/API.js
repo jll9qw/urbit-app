2fd0cfbb3d460249cc82e5b1304f1f4af5817e1e
Admin <admin@MacBook-Pro.local>
2019-07-06 11:23:32 -0400

    updated utills

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  // Gets all herbs
+  getHerbs: function() {
+    return axios.get("/api/herbs");
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+  // Gets the herb with the given id
+  getHerb: function(id) {
+    return axios.get("/api/herbs/" + id);
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+  // Deletes the herb with the given id
+  deleteHerb: function(id) {
+    return axios.delete("/api/herbs/" + id);
   },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+  // Saves a herb to the database
+  saveHerb: function(herbData) {
+    return axios.post("/api/herbs", herbData);
   }
 };
