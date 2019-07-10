160cc12859fa11d9cb6c89afa1f50d06137b240c
jll9qw <lloyd804@gmail.com>
2019-07-09 21:03:11 -0400

    added footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function Footer() {
+    return (
+       <footer class="d-flex justify-content-center py-4 py-md-2 ui_gradient2">
+	<div class="container px-0 d-flex flex-column justify-content-center py-2">
+		
+		<ul class="nav flex-column flex-md-row justify-content-center align-items-center px-0">
+  <li class="nav-item mb-3 mb-md-0">
+    <a class="nav-link align-self-center rounded-btn shadow-sm px-3 active" href="#" style="color: #43c6ac;">hrbs</a>
+  </li>
+  <li class="nav-item mb-3 mb-md-0">
+    <a class="nav-link disabled text-light" href="#">&copy;2019 MaurJaso</a>
+  </li>
+  <li class="nav-item mb-3 mb-md-0">
+  </li>
+  <li class="nav-item d-flex w-25- mb-3 mb-md-0 justify-content-center">
+		<a class="btn-light- btn-white rounded-btn my-lg-2 p-2 shadow text-pinkish" href="#"><i class="fab fa-twitter"></i></a>
+				<a class="btn-light- btn-white rounded-btn mx-3 my-lg-2 p-2 shadow text-pinkish" href="#"><i class="fab fa-facebook"></i></a>
+				<a class="btn-light- btn-white rounded-btn my-lg-2 p-2 shadow text-pinkish" href="#"><i class="fab fa-instagram"></i></a>
+  </li>
+</ul>
+		
+	</div>
+</footer>
+    );
+  }
+
+
+  
+
+export default Footer;
