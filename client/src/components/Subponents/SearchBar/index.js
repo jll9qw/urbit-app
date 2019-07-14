80858946aadc35b6056b3918e80e6a977ca6201e
MauriceWebb <maurice.webb91@gmail.com>
2019-07-13 21:55:19 -0400

    add functionality to search bar...

--- client/src/components/Subponents/SearchBar/index.js
+++ client/src/components/Subponents/SearchBar/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import $ from 'jquery';
+
+// Functions...
+const handleSearch = _=> {
+  const query = $('#searchBarInput').val().trim();
+  console.log('Searching for: ', query);
+}
 
 const SearchBar = (props) => {
     return (
@@ -11,11 +18,14 @@ const SearchBar = (props) => {
               <input
                 type="text"
                 className="form-control border-0 px-4"
+                id='searchBarInput'
                 aria-label="Text input with segmented search button"
                 style={{ outline: "none", color: "#408468", lineHeight: 10 }}
               />
               <div className="input-group-append">
-                <a className="btn ui_btn2 border-0 text-white">
+                <a className="btn ui_btn2 border-0 text-white"
+                onClick={_=> handleSearch()}
+                >
                   <i className="fas fa-search px-4" />
                 </a>
               </div>
