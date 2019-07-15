d8a18975df74653703214231de642a182d2e7851
MauriceWebb <maurice.webb91@gmail.com>
2019-07-14 22:50:17 -0400

    removed unneccessary scripts...

--- client/public/scripts.js
+++
@@ -1,9 +0,0 @@
-import $ from "jquery";
-
-$(document).ready(_ => {
-//   console.log("this is working!!!!");
-
-  $("#myModal").on("shown.bs.modal", function() {
-    // $("#myInput").trigger("focus");
-  });
-});
