5707669a2a79485ed539d719255c917912d0cd42
Admin <admin@MacBook-Pro.local>
2019-07-18 20:44:01 -0400

    created a db for users. Added a new script for seeding db.Users...

---
+++ controllers/usersController.js
@@ -0,0 +1,47 @@
+const db = require("../models");
+
+// Defining methods for the UsersController
+module.exports = {
+  findAll: function(req, res) {
+    db.Users.find(req.query)
+      // .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findUserByEmail: function(req, res) {
+    db.Users.find({email: req.query})
+      // .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+//   findById: function(req, res) {
+//     db.Users.findById(req.params.id)
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
+//   searchByTerm: (req, res) => {
+//     db.Users.find(
+//       { $text: { $search: req.params.query } },
+//       { score: { $meta: "textScore" } }
+//     )
+//       .sort({ score: { $meta: "textScore" } })
+//       .then(dbModel => res.json(dbModel))
+//       .catch(err => res.status(422).json(err));
+//   },
+  create: function(req, res) {
+    db.Users.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Users.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Users.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
