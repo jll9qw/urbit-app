5ae7def8b48afb30c61d0a1a1de310d5d2d88f03
jll9qw <lloyd804@gmail.com>
2019-07-20 00:59:21 -0400

    merged content

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import Nav from "../subponents/Nav/index";
-import Jumbotron from "../subponents/Jumbotron/index.js";
-import SearchBar from "../subponents/SearchBar/index";
+import Nav from "../subcomponent/Nav/index";
+import Jumbotron from "../subcomponent/Jumbotron/index.js";
+import SearchBar from "../subcomponent/SearchBar/index";
 import "./style.css";
 
 class Header extends Component {
@@ -10,139 +10,9 @@ class Header extends Component {
     return (
       <div>
         <header className="d-flex flex-column justify-content-around py-2 ui_gradient2">
-<<<<<<< HEAD
-          <div className="container px-0">
-            <nav className="navbar navbar-expand-lg navbar-light d-flex align-items-stretch">
-              <a
-                className="navbar-brand align-self-center rounded-btn shadow-sm px-3 active"
-                href="#"
-                style={{ color: "#43c6ac" }}
-              >
-                URBIT<span className="sr-only">(current)</span>
-              </a>
-              <button
-                className="navbar-toggler border-0 shadow rounded-btn px-3"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                style={{ outline: "none" }}
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
-
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav text-center ml-lg-auto d-flex justify-content-between align-items-center">
-                  <li className="nav-item mx-1">
-                    <a
-                      className="nav-link rounded-btn mb-3 my-lg-2 px-4 px-md-4"
-                      href="#"
-                    >
-                      herbs
-                    </a>
-                  </li>
-                  <li className="nav-item mx-1">
-                    <a
-                      className="nav-link rounded-btn mb-3 my-lg-2 px-4 px-md-4"
-                      href="#"
-                    >
-                      forum
-                    </a>
-                  </li>
-                  <li className="nav-item mx-1">
-                    <a
-                      className="nav-link rounded-btn mb-3 my-lg-2 px-4 px-md-4"
-                      href="#"
-                    >
-                      about us
-                    </a>
-                  </li>
-                  <li className="nav-item mx-1">
-                    <a
-                      className="nav-link rounded-btn mb-3 my-lg-2 px-4 px-md-4"
-                      href="#"
-                    >
-                      blog
-                    </a>
-                  </li>
-                  <li className="nav-item  mx-1">
-                    <a
-                      className="nav-link btn btn-light rounded-btn mb-3 my-lg-2 px-4 px-md-4 shadow"
-                      href="#"
-                    >
-                      log in
-                    </a>
-                  </li>
-                  <li className="nav-item mx-0 ml-lg-3">
-                    <a
-                      className="nav-link btn ui_btn2 border-0 my-lg-2 text-white rounded-btn px-4 shadow"
-                      href="#"
-                    >
-                      subscribe
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-
-          <div className="container mt-4 mt-3- mt-md-n5 mt-lg-n3">
-            <div className="row mb-n5-">
-              <div className="col-md-4 d-flex justify-content-center align-items-center mb-n5- mb-md-0 border-danger">
-                <div className="container text-center- text-right text-md-left mb-3 mb-sm-0 px-0">
-                  <div className="row">
-                    <div className="col-6 col-md-12">
-                      <h2>Welcome to URBIT</h2>
-                    </div>
-                    <div className="col-6 col-md-12 d-flex flex-column">
-                      <p className="text-left">
-                      A hub for alternative health solutions and metaphysical healing
-                      </p>
-                      <a
-                        href="#"
-                        className="btn rounded-btn shadow-sm ui_btn1 text-white border-0 mx-auto- mr-auto mx-md-0 px-3 py-2 px-md-0"
-                      >
-                        See More
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-md-8 header_image mt-n5- mt-sm-n4-"
-                style={{minHeight: 350}}
-              />
-            </div>
-          </div>
-
-          <div className="container">
-            <div
-              className="input-group mb-2 shadow-sm rounded-btn overflow-hidden border-0"
-              style={{ outline: "none" }}
-            >
-              <input
-                type="text"
-                className="form-control border-0 px-4"
-                aria-label="Text input with segmented search button"
-                style={{ outline: "none", color: "#408468", lineHeight: 10 }}
-              />
-              <div className="input-group-append">
-                <a className="btn ui_btn2 border-0 text-white">
-                  <i className="fas fa-search px-4" />
-                </a>
-              </div>
-            </div>
-          </div>
-=======
           <Nav />
           <Jumbotron />
           <SearchBar />
->>>>>>> 311b2bbef2a44942f2cc9d1b5ed61bd66962304a
         </header>
       </div>
     );
--- client/src/components/Main/index.js
+++ client/src/components/Main/index.js
@@ -1,126 +1,13 @@
 import React, { Component } from 'react';
 // Subponents...
-import Carousel from '../subponents/Carousel/index';
-import MediaObject1 from '../subponents/MediaObject1/index';
-import CardDeck from '../subponents/CardDeck/index';
-import MediaObject2 from '../subponents/MediaObject2/index';
+import Carousel from '../subcomponent/Carousel/index';
+import MediaObject1 from '../subcomponent/MediaObject1/index';
+import CardDeck from '../subcomponent/CardDeck/index';
+import MediaObject2 from '../subcomponent/MediaObject2/index';
 
 import './style.css';
 import './functionality';
 
-<<<<<<< HEAD
-	<div className="container rounded">
-		<div className="row d-flex">
-			<div className="col-md-6 rounded">
-				<div className="container herbal_bowl_image2"></div>
-			</div>
-			<div className="col-md-6 py-3 d-flex justify-content-center align-items-center rounded">
-				<div className="container">
-					<div className="row">
-						<div className="col-12 text-center text-md-left">
-							<h3>A hub for alternative health solutions and metaphysical healing</h3>
-						</div>
-						<div className="col-12">
-							<div className="container pl-0">
-								<hr className="w-100 bg-success rounded py-1 ui_gradient2 border-0 mt-n1"/>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12">
-							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, quidem voluptatibus rerum laudantium reprehenderit totam enim voluptas quae dolore quasi vero, facere perspiciatis, doloremque velit.</p>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div className="container shadow-sm- rounded p-3">
-		<h3 className="text-center">The healthy way.</h3>
-		<hr className="w-25 bg-success rounded py-1 ui_gradient2 border-0 mt-n1"/>
-		<div className="row">
-			<div className="container card-deck">
-				
-				<div className="card border-0 card_fx round_corner overflow-hidden">
-					<img src="http://static1.squarespace.com/static/58fd210246c3c4c4320684ae/58fee05b29687f9157642b14/595a88c4440243407b761085/1499106620063/irish-moss-benefits-fresh-seaweed.jpg?format=1500w" className="card-img-top" alt="sea moss health drink"/>
-					<div className="card-body">
-						<h5 className="card-title">Add this to your diet</h5>
-						<p className="card-text">
-Sea moss drink recipe – optimise your health by learning to make this refreshingly sweet spiced infused Jamaican Irish moss drink.</p>
-						<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-					</div>
-				</div>
-
-				<div className="card border-0 card_fx round_corner overflow-hidden">
-					<img src="https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/turmeric_slideshow/1800ss_thinkstock_rf_fresh_and_dried_turmeric_roots.jpg?resize=650px:*" className="card-img-top" alt="tumeric"/>
-					<div className="card-body">
-						<h5 className="card-title">Home remedies for lower Cholesterol 
-</h5>
-						<p className="card-text">There are mainly two types of cholesterol-Low-Density Lipoproteins (LDL) which is the bad cholesterol that increases your risk of heart disease and the High-Density Lipoproteins (HDL) which is the good cholesterol that reduces your risk of heart diseases.</p>
-						<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-					</div>
-				</div>
-
-				<div className="card border-0 card_fx round_corner overflow-hidden">
-					<img src="https://source.unsplash.com/random/300x200?herbal" className="card-img-top" alt="..."/>
-					<div className="card-body">
-						<h5 className="card-title">Meditation is the best high ever</h5>
-						<p className="card-text">When you allow yourself to sit quietly and just "be," you can experience a feeling that goes beyond a state of mind you're used to. It can feel boundless or limitless, and can best be described as transcendent.</p>
-						<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
-	<div className="container rounded">
-		<div className="row d-flex">
-			<div className="col-md-6 py-3 d-flex justify-content-center align-items-center rounded">
-				<div className="container">
-					<div className="row">
-						<div className="col-12 text-center text-md-left">
-							<h2>Fun Fact</h2>
-						</div>
-						<div className="col-12">
-							<div className="container pl-0">
-								<hr className="w-100 bg-success rounded py-1 ui_gradient2 border-0 mt-n1"/>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12">
-							<h6>Garlic was used as an antiseptic against gangrene in the First World War.</h6>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="col-md-6 herbal_bowl_image rounded"></div>
-		</div>
-	</div>
-
-	<div className="container rounded">
-		<div className="row d-flex">
-			<div className="col-md-6 holistic_image rounded"></div>
-			<div className="col-md-6 py-3 d-flex justify-content-center align-items-center rounded">
-				<div className="container">
-					<div className="row">
-						<div className="col-12 text-center text-md-left">
-							<h2>In the News</h2>
-						</div>
-						<div className="col-12">
-							<div className="container pl-0">
-								<hr className="w-100 bg-success rounded py-1 ui_gradient2 border-0 mt-n1"/>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="col-12">
-							<p><a href="https://www.forbes.com/sites/johnlamattina/2018/06/20/there-go-those-drug-companies-inventing-new-diseases-again/#5f0114df34bb">"So, if current events weren’t enough to scare you, now you have to be on the lookout for “malady mongers” – drug companies that sell pills by inventing disease. We have Michael Walsh to thank for bringing this to the public’s attention." - Forbes</a></p>
-						</div>
-					</div>
-				</div>
-=======
 class Main extends Component {
 	state = {};
 	render() {
@@ -139,7 +26,6 @@ class Main extends Component {
 					<MediaObject1 />
 					
 				</main>
->>>>>>> 311b2bbef2a44942f2cc9d1b5ed61bd66962304a
 			</div>
 		);
 	}
